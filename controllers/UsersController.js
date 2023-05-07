67c1eb4561c20c0b638256cab82bf956879c67f4
Frederico Cassola <cfcassola@gmail.com>
2023-05-07 08:38:23 +0200

    Add DELETE not fished

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -73,4 +73,22 @@ const updateUser = (req, res) => {
     });
 };
 
-module.exports = { getUsers, getUserById, postUsers, updateUser };
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("DELETE FROM users WHERE id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error editing the user");
+    });
+};
+
+module.exports = { getUsers, getUserById, postUsers, updateUser, deleteUser };
